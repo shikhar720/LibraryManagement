cd6c599dd5fd86daa4c3a7805e7f19b0a9f558b2
shikhar720 <53426020+shikhar720@users.noreply.github.com>
2022-08-26 14:39:12 +0530

    Update SpringJunitApplication.java

--- SpringJunit/src/main/java/com/example/demo/SpringJunitApplication.java
+++ SpringJunit/src/main/java/com/example/demo/SpringJunitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJunitApplication {
-
+//remote changes
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJunitApplication.class, args);
 	}
