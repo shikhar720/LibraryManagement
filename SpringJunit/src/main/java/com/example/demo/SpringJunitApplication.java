403dcfe9b8ecedaaddf5e45d229c144ab5fdc518
Shikhar Gupta <shigupta@axway.com>
2022-08-26 16:22:59 +0530

    comment change

--- SpringJunit/src/main/java/com/example/demo/SpringJunitApplication.java
+++ SpringJunit/src/main/java/com/example/demo/SpringJunitApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJunitApplication {
-
+// Local Changes
+	//nfjfjfjf
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJunitApplication.class, args);
 	}
